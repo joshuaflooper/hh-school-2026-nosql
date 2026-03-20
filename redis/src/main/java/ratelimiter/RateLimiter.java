4c0ea8091716080ef218eb3b42b70d7e0c59703d
Georgiy Podobin <gpodobin@yandex.ru>
2026-03-20 19:04:51 +0300

    Complete task 1 (coding part)

--- redis/src/main/java/ratelimiter/RateLimiter.java
+++ redis/src/main/java/ratelimiter/RateLimiter.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.Instant;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
@@ -13,18 +14,60 @@ public class RateLimiter {
   private final String label;
   private final long maxRequestCount;
   private final long timeWindowSeconds;
+  private final long timeWindowMilli;
 
   public RateLimiter(Jedis redis, String label, long maxRequestCount, long timeWindowSeconds) {
     this.redis = redis;
     this.label = label;
     this.maxRequestCount = maxRequestCount;
     this.timeWindowSeconds = timeWindowSeconds;
+    timeWindowMilli = timeWindowSeconds * 1000; // Чтобы не умножать каждый раз
   }
 
+// Скользящий журнал из статьи?
+
   public boolean pass() {
-    // TODO: Implementation
-    return false;
+    if (maxRequestCount == 0) {
+      return false;
+    }
+
+    String key = "request:%s".formatted(label);
+    String value;
+    long now = Instant.now().toEpochMilli();
+
+    value = redis.lindex(key, maxRequestCount - 1);
+
+    if (value != null
+        && now - Long.parseLong(value) < timeWindowMilli) {
+      return false;
+    }
+
+    redis.lpush(key, String.valueOf(now));
+    return true;
   }
+
+// Ниже моя первая реализация.
+// Фактически это фиксированное окно из статьи, поэтому тест для скользящего окна она не проходит.
+
+//  public boolean pass() {
+//    if (maxRequestCount == 0) {
+//      return false;
+//    }
+//
+//    String key = "request:%s".formatted(label);
+//    String value;
+//
+//    value = redis.get(key);
+//
+//    if (value != null
+//        && Long.parseLong(value) >= maxRequestCount) {
+//      return false;
+//    }
+//
+//    redis.incr(key);
+//    redis.expire(key, timeWindowSeconds, ExpiryOption.NX);
+//    return true;
+//  }
 
   public static void main(String[] args) {
     JedisPool pool = new JedisPool("localhost", 6379);
