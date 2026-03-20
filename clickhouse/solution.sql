8e0a4f8df835ba6c7e9a4a02e5631c8b1e101a63
Georgiy Podobin <gpodobin@yandex.ru>
2026-03-20 23:56:34 +0300

    Complete task 3

--- clickhouse/solution.sql
+++ clickhouse/solution.sql
@@ -1,22 +1,37 @@
 -- Решение заданий по ClickHouse
 
--- 1. Создание таблицы
--- TODO: скопируйте и доработайте CREATE TABLE из schema.sql
+CREATE TABLE IF NOT EXISTS server_logs (
+    timestamp DateTime,
+    user_id UInt16,
+    endpoint String,
+    response_time_ms UInt16,
+    status_code UInt16
+) ENGINE = MergeTree()
+ORDER BY (endpoint);
 
 
--- 2. Загрузка данных из CSV
--- Подсказка: можно использовать clickhouse-client с параметром --query
--- Пример команды (выполняется в терминале):
--- cat server_logs.csv | clickhouse-client --query="INSERT INTO server_logs FORMAT CSVWithNames"
+-- Выполнил следующую команду, чтобы вставить данные:
+-- cat server_logs.csv | docker exec -i clickhouse clickhouse-client --query="INSERT INTO server_logs FORMAT CSVWithNames"
 
 
 -- 3. Запрос: Топ-5 самых медленных endpoint'ов (по среднему времени ответа)
--- TODO: напишите SELECT запрос
+SELECT endpoint, avg(response_time_ms) as avg_response_time_ms
+FROM server_logs
+GROUP BY endpoint
+ORDER BY avg_response_time_ms DESC
+LIMIT 5;
 
 
 -- 4. Запрос: Количество запросов по часам за весь период в логах
--- TODO: напишите SELECT запрос с использованием функции toHour() или formatDateTime()
+SELECT toStartOfHour(timestamp) AS hour, count(*) AS count
+FROM server_logs
+GROUP BY hour
+ORDER BY hour DESC;
 
 
 -- 5. Запрос: Процент ошибок (status_code >= 400) для каждого endpoint'а
--- TODO: напишите SELECT запрос с вычислением процента ошибок
+SELECT 
+    endpoint, 
+    count(CASE WHEN status_code >= 400 THEN 1 ELSE NULL END) / count(*) * 100 AS errors_percentage
+FROM server_logs
+GROUP BY endpoint;
